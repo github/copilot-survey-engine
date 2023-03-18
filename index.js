a5de17b9974fae1ce695c2797e7472f65a4f84e5
Mabel Geronimo <107436170+mageroni@users.noreply.github.com>
2023-03-18 07:04:41 -0500

    working version - pull request closed

--- index.js
+++ index.js
@@ -2,15 +2,33 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
+
+const dedent = require('dedent');
+const fs = require('fs');
+const path = require("path");
+
+const issue_body = fs.readFileSync('./issue_template/copilot-usage-spa.md', 'utf-8');
+
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+  app.on("pull_request.closed", async (context) => {
+    let pr_number = context.payload.pull_request.number;
+
+    // find XXX in file and replace with pr_number
+    let fileContent = dedent(issue_body.replace(/XXX/g, '#'+pr_number.toString()) );
+    
+    // display the body for the issue
+    app.log.info(fileContent);
+
+    // create an issue using fileContent as body
+    await context.octokit.issues.create({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      title: "Copilot Usage",
+      body: fileContent,
     });
-    return context.octokit.issues.createComment(issueComment);
   });
 
   // For more information on building apps:
---
+++ issue_template/copilot-usage-spa.md
@@ -0,0 +1,14 @@
+### Encuesta sobre el uso de Copilot
+
+Hey! 👋  Como parte de nuestros esfuerzos por mejorar continuamente nuestros procesos de DevOps, nos gustaría recopilar sus valiosos comentarios sobre su experiencia con Copilot para el Pull Request XXX a través de las siguientes preguntas:
+
+1. ¿Copilot ahorró tiempo o aumentó la productividad en el desarrollo de este componente?
+- [ ] Si
+- [ ] No
+2. Si la respuesta 1 es Si, ¿De cuánto fue la mejora (5 %, 10 %, 20 %, 30 % o más)? 
+- [ ] <5%
+- [ ] 5%-10%
+- [ ] 10%-20%
+- [ ] 20%-30%
+- [ ] >30%
+4. Si la respuesta 1 es No, Explique por qué en un comentario
